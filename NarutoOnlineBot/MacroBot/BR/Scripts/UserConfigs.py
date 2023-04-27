fcde9b5bc00ecd8c5b769c4eef5936fb5e32ddd4
Nicolas <102241685+nicolasTdC@users.noreply.github.com>
2023-04-27 20:41:22 -0300

    Create UserConfigs.py

---
+++ NarutoOnlineBot/MacroBot/BR/Scripts/UserConfigs.py
@@ -0,0 +1,50 @@
+
+####
+
+filePath = r'C:\Users\accam\Desktop\NarutoOnline\NarutoOnlineBot\BR'
+
+#eventos True se ainda precisa fazer
+
+#arena True se quiser que faca
+
+#Convoy = True
+
+Dono = 'True' #valor de ryos
+
+#RichField=True
+faseRichField = 'Normal'
+
+Roleta='True' 
+
+#CheckIn=True
+
+Recruit = 'True'
+
+#NinjaExam = True
+
+LuckyRunes = 'True'
+ 
+#SurvivalTrial=True
+pointsSurvivalTrial = 130  
+
+
+#ExpTreasure=True
+faseExpTreasure = 'Sakura'
+
+
+#NinjaTest= True
+
+Matsuri='True'
+Matsuri2='True'
+
+##
+NineTails = 'False'
+
+#TeamInstance = True
+faseTI= 'Gaara'
+difTI = 'Easy'
+
+
+Arena=False
+
+#%%
